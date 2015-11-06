758d0b58d419619dd4060573be8f649d9f65bb53
drammock <dan.mccloy@gmail.com>
2015-11-05 17:52:51 -0800

    first commit of figures

---
+++ figures/fig-1.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+===============================================================================
+Script 'fig-1.py'
+===============================================================================
+
+This script plots a trial diagram for the pupil vocoder switching task.
+"""
+# @author: Dan McCloy (drmccloy@uw.edu)
+# Created on Wed Sep 23 16:57:41 2015
+# License: BSD (3-clause)
+
+
+from numpy import linspace
+import matplotlib.pyplot as plt
+from convenience_functions import use_font
+
+plt.ioff()
+use_font('source')
+
+# set up figure
+fig = plt.figure(figsize=(6.5, 1.75))
+ax = plt.Axes(fig, [0.025, 0.25, 0.9, 0.75])
+ax.axis('off')
+fig.add_axes(ax)
+
+# color defs
+gapcol = '#999933'
+malecol = '0.6'
+femalecol = '0.75'
+cuecol = 'w'
+maintcol = '#332288'
+switchcol = '#aa4499'
+lettercol = 'w'
+slashcol = 'k'
+
+# maint / switch lines
+ax.plot((1, 4.5), (2.1, 2.1), color=maintcol, linewidth=3.5,
+        solid_capstyle='butt')
+ax.plot((1, 2.5, 3.1, 4.5), (1.9, 1.9, 0, 0), color=switchcol,
+        linewidth=3.5, linestyle='--')
+ax.text(4.6, 2.1, 'maintain', color=maintcol, ha='left', va='center')
+ax.text(4.6, 0, 'switch', color=switchcol, ha='left', va='center')
+
+# boxes & letters
+centers_x = [0.23, 0.75, 1.75, 2.25, 3.35, 3.85, 1.75, 2.25, 3.35, 3.85]
+centers_y = [2] * 6 + [0] * 4
+box_x = [(0, 0, 1, 1)] + [(1.5, 1.5, 2.5, 2.5), (3.1, 3.1, 4.1, 4.1)] * 2
+box_y = [(1.5, 2.5, 2.5, 1.5)] * 3 + [(-0.5, 0.5, 0.5, -0.5)] * 2
+box_l = ['AA', 'AU', 'E', 'O', 'P', 'O', 'G', 'V', 'E', 'U']  # ADEGOPUV
+color = [maintcol, switchcol] + [lettercol] * 8
+bcolor = [cuecol] + [malecol] * 2 + [femalecol] * 2
+ecolor = ['k'] + ['none'] * 4
+wt = ['normal'] * 2 + ['bold'] * 8  # ['bold'] * 10
+ha = ['left', 'right'] + ['center'] * 8
+for x, y, b, e in zip(box_x, box_y, bcolor, ecolor):
+    ax.fill(x, y, b, alpha=1, zorder=4, edgecolor=e, linewidth=0.5)
+for x, y, s, c, h, w in zip(centers_x, centers_y, box_l, color, ha, wt):
+    ax.text(x, y - 0.04, s, ha=h, va='center', color=c, weight=w, zorder=5)
+ax.text(0.5, 2, '/', color=slashcol, ha='center', va='center', zorder=5)
+
+# switch gap
+rect = plt.Rectangle((2.5, -2.8), width=0.6, height=0.5, zorder=4,
+                     fill=False, linewidth=0.5, edgecolor=gapcol)
+yy = [(-2.55, -2.8)] + [(-2.3, -2.8)] * 5 + [(-2.3, -2.55)]
+xx = [(2.5, 2.6)] + [(x, x + 0.2) for x in linspace(2.5, 2.9, 5)] + [(3, 3.1)]
+_ = [plt.plot(x, y, linewidth=0.5, color=gapcol, solid_capstyle='butt',
+              zorder=4) for x, y in zip(xx, yy)]
+ax.add_artist(rect)
+ax.set_clip_on(False)
+ax.text(2.8, -3.3, 'variable gap\n(200 ms or 600 ms)', ha='center',
+        va='top', fontsize=10, color=gapcol)
+
+# captions
+ax.text(0.5, 3.5, 'Cue', color='k', fontsize=11, ha='center', va='center')
+ax.text(2.8, 3.5, 'Concurrent target and masker streams', color='k',
+        fontsize=11, ha='center', va='center', weight='normal')
+ax.text(1.5, 2.55, 'male', color=malecol, fontsize=9, ha='left', va='baseline',
+        weight='bold')
+ax.text(1.5, 0.55, 'female', color=femalecol, fontsize=9, ha='left',
+        va='baseline', weight='bold')
+
+# timeline
+arr_y = -2.3
+arr_xmax = 4.7
+tcklen = 0.25
+ticktimes = [0, 1, 1.5, 2.5, 3.1, 4.1]
+ticklabels = [str(tt) for tt in ticktimes]
+ticklabels[-2:] = [str(a - 0.4) + ' or ' + str(a) for a in ticktimes[-2:]]
+_ = ax.vlines(ticktimes, arr_y - tcklen, arr_y + tcklen, linewidths=0.5,
+              zorder=5)
+_ = [ax.text(x, y, s, ha='center', va='baseline', fontsize=9)
+     for x, y, s in zip(ticktimes, [arr_y + 2 * tcklen] * len(ticktimes),
+                        ticklabels)]
+arr = ax.arrow(-0.2, arr_y, arr_xmax, 0, head_width=0.4, head_length=0.15,
+               fc='k', ec='k', linewidth=0.5, zorder=5)
+plt.annotate('time (s)', (arr_xmax, arr_y),  xytext=(3, 0), fontsize=9,
+             textcoords='offset points', ha='left', va='center')
+
+# finalize
+plt.ylim(-2.8, 4.8)
+plt.xlim(-0.1, 5)
+fig.savefig('fig-1.pdf')
---
+++ figures/fig-2.py
@@ -0,0 +1,240 @@
+# -*- coding: utf-8 -*-
+"""
+===============================================================================
+Script 'fig-2.py'
+===============================================================================
+
+This script plots pupil size & significance tests for the vocoder experiment.
+"""
+# @author: Dan McCloy (drmccloy@uw.edu)
+# Created on Fri Sep 25 11:15:34 2015
+# License: BSD (3-clause)
+
+import os.path as op
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import colorConverter as cc
+from scipy.stats import distributions
+from mne.stats import spatio_temporal_cluster_1samp_test, ttest_1samp_no_p
+from convenience_functions import (box_off, use_font, tick_label_size,
+                                   hatch_between)
+from functools import partial
+
+# mostly rcParams stuff
+plt.ioff()
+use_font('source')
+tick_label_size(10)
+
+# flags
+plot_stderr = True
+plot_signif = True
+show_pval = False
+savefig = True
+continuous_deconv = False
+
+# file I/O
+work_dir = '..'
+voc_file = op.join(work_dir, 'voc_data.npz')
+#rev_file = op.join(work_dir, 'rev_data.npz')
+vv = np.load(voc_file)
+#rr = np.load(rev_file)
+data_deconv, t_fit, subjects = vv['fits'], vv['t_fit'], vv['subjects']
+data_zscore, fs, kernel = vv['zscores'], vv['fs'], vv['kernel']
+#data_rev, t_fit_rev = rr['fits'], rr['t_fit']
+if continuous_deconv:
+    data_cont, t_cont = vv['fits_cont'], vv['t_cont']
+'''
+data_zscore.shape
+16,   40,      2,          2,            2,         6550
+subj  trials   200/600gap  maint/switch  10/20chan  samples
+'''
+
+# params
+stim_times = np.array([0, 0.5, 1.5, 2.0, 2.5, 3.0])  # gap not included (yet)
+stim_dur = 0.47  # really 0.5, but leave a tiny gap
+peak = np.where(kernel == kernel.max())[0][0] / float(fs)
+t_min, t_max = -0.5, 6. - peak
+t_zs = t_min + np.arange(data_zscore.shape[-1]) / float(fs)
+stat_fun = partial(ttest_1samp_no_p, sigma=1e-3)
+
+# colors
+#cue, msk, blu, red = '0.5', '0.75', '#332288', '#aa4499'
+cue, msk, blu, red = '0.4', '0.65', '#332288', '#aa4499'
+grn, yel = '#44aa99', '#ddcc77'
+signifcol = '0.95'
+axiscol = '0.8'
+tickcol = '0.8'
+axislabcol = '0.3'
+ticklabcol = '0.5'
+
+# set up figure
+fig, axs = plt.subplots(3, 1, figsize=(3, 6.5))
+xlim = [t_min, t_max]
+#signifs = list()
+
+for t, data in zip([t_fit], [data_deconv]):
+    # collapse across trials and experimental contrasts
+    # axis 1 is trials, 2 is gap dur, 3 is maint/switch, 4 is num voc channels
+    chan_10_vs_20 = np.nanmean(data, axis=(1, 2, 3))
+    gap_200_vs_600 = np.nanmean(data, axis=(1, 3, 4))
+    maint_vs_switch = np.nanmean(data, axis=(1, 2, 4))
+    # axis limits
+    ymax = np.ceil(np.max(np.mean(np.nanmean(data_deconv, axis=1), axis=0)))
+    ylim = [-0.6 * ymax, ymax]
+    for jj, (contrast, ax) in enumerate(zip([chan_10_vs_20, gap_200_vs_600,
+                                             maint_vs_switch], axs)):
+        # TRIAL TIMECOURSE
+        labels = [['10 band', '20 band/'], ['short gap', 'long gap'],
+                  ['maintain', 'switch']][jj]
+        colors = [[cue, cue], [cue, cue], [blu, red]][jj]
+        gaps = [[0.6, 0.6], [0.2, 0.6], [0.6, 0.6]][jj]
+        # y values for stim timecourse diagram
+        stim_ymin = ymax * -0.45
+        stim_ymax = ymax * -0.3
+        stim_ymid = (stim_ymin + stim_ymax) / 2.
+        yvals = [[stim_ymid, stim_ymid], [stim_ymin, stim_ymax],
+                 [stim_ymin, stim_ymax]][jj]
+        thk = 0.0125 * ymax
+        off = 0.05 * ymax
+        for kk in [0, 1]:
+            col = colors[kk]
+            gap = gaps[kk]
+            textcol = [blu, red][kk]
+            stim_y = yvals[kk]
+            stim_c = [cue] * 2 + [col] * 4
+            stim_m = [msk] * 4
+            if jj == len(axs) - 1:  # maint / switch
+                stim_c = [cue] * 2 + [col] * 2 + [[col] * 2, [msk] * 2][kk]
+                stim_m = [msk] * 2 + [[msk] * 2, [col] * 2][kk]
+            stim_t = stim_times + np.array([0] * 4 + [gap] * 2)
+            if jj != 0 or kk != 1:  # only draw once for 10/20 chan plot
+                # cue and attended stims
+                for tt, cl in zip(stim_t, stim_c):
+                    stim_x = (tt, tt + stim_dur)
+                    _ = ax.fill_between(stim_x, stim_y+thk, stim_y-thk,
+                                        color=cl, edgecolor='none',
+                                        zorder=9)
+                # masker stims
+                for tt, mk in zip(stim_t[2:], stim_m):
+                    stim_x = (tt, tt + stim_dur)
+                    _ = ax.fill_between(stim_x, stim_y+thk-off,
+                                        stim_y-thk-off, color=mk,
+                                        edgecolor='none', zorder=9)
+            # timecourse labels
+            lab = labels[kk]
+            ytxt = [stim_ymin, stim_ymax][kk]
+            xyt = [(-6, 0), (-6, 0), (-6, 0)][jj]
+            _ = ax.annotate(lab, (0, ytxt), xytext=(-6, 0),
+                            textcoords='offset points', color=textcol,
+                            ha='right', va='center', fontsize=9,
+                            fontstyle='italic')
+        # cue label
+        _ = ax.annotate('cue', xy=(stim_times[1], stim_y + thk),
+                        xytext=(0, 1.5), textcoords='offset points',
+                        fontsize=9, fontstyle='italic', ha='center',
+                        va='bottom', color=cue)
+        # within-subject difference between conditions
+        contr_diff = (contrast[:, 1, :] - contrast[:, 0, :])[:, :, np.newaxis]
+        # collapse across subjects (only for plotting, not stats)
+        contr_std = np.std(contrast, axis=0) / np.sqrt(len(contrast) - 1)
+        contr_mean = np.mean(contrast, axis=0)
+        # plot curves
+        for mm, (cond, se) in enumerate(zip(contr_mean, contr_std)):
+            col = [blu, red][mm]
+            tcol = cc.to_rgb(col) + (0.4,)  # add alpha channel
+            zord = [2, 0][mm]
+            # plot standard error bands
+            if plot_stderr:
+                _ = ax.fill_between(t, cond - se, cond + se, color=tcol,
+                                    edgecolor='none', zorder=zord + 2)
+            # plot mean lines
+            _ = ax.plot(t, cond, color=col, linewidth=1.5, zorder=zord + 3)
+        # stats
+        if plot_signif:
+            thresh = -1 * distributions.t.ppf(0.05 / 2, len(contr_diff) - 1)
+            result = spatio_temporal_cluster_1samp_test(
+                contr_diff, threshold=thresh, stat_fun=stat_fun, n_jobs=6,
+                buffer_size=None, n_permutations=np.inf)
+            tvals, clusters, cluster_pvals, H0 = result
+            signif = np.where(np.array([p <= 0.05 for p in cluster_pvals]))[0]
+            signif_clusters = [clusters[s] for s in signif]
+            signif_cluster_pvals = cluster_pvals[signif]
+            # plot stats
+            for clu, pv in zip(signif_clusters, signif_cluster_pvals):
+                '''
+                # this index tells direction of tval, hence could be used to
+                # decide which color to draw the significant cluster region
+                # based on which curve is higher:
+                idx = (np.sign(tvals[clu[0][0], 0]).astype(int) + 1) // 2
+                '''
+                clu = clu[0]
+                cluster_ymin = ylim[0] * np.ones_like(t[clu])
+                cluster_ymax = np.max(contr_mean[:, clu], axis=0)  # under top
+                pval_x = t[int(np.mean(clu[[0, -1]]))]
+                pval_y = -0.1 * ylim[1]
+                pval_ord = np.trunc(np.log10(pv)).astype(int)
+                _ = hatch_between(ax, 9, t[clu], cluster_ymin,
+                                  cluster_ymax, bgcolor=signifcol,
+                                  linewidth=1.25, color='w', zorder=1)
+                if show_pval:
+                    pval_txt = '$p < 10^{{{}}}$'.format(pval_ord)
+                    _ = ax.text(pval_x, pval_y, pval_txt, ha='center',
+                                va='baseline', fontdict=dict(size=10))
+                #signifs.append([ax, t, clu, cluster_ymin, cluster_ymax])
+        '''
+        # vertical lines
+        if plot_signif and ii == 0:
+            ax.plot((t[clu][0], t[clu][0]), (cluster_ymin[0], cluster_ymax[0]),
+                    linestyle=':', color='k')
+        else:
+            # arrows
+            arrow_col = [grn, yel][ii - 1]
+            dx = 0.05
+            dy = 0.025 * ymax
+            arrow_x = t[clu][0]
+            arrow_y = ylim[0] - 7*dy
+            arrow_dx = 0
+            arrow_dy = 6*dy
+            arr = ax.arrow(arrow_x, arrow_y, arrow_dx, arrow_dy, width=dx,
+                           head_width=5*dx, head_length=3*dy, fc=arrow_col,
+                           ec=(0., 0., 0., 0.2), linewidth=0.5, clip_on=False,
+                           length_includes_head=True)
+        '''
+
+        # set axis limits
+        xlim[1] = 1.001 * xlim[1]
+        ylim[1] = 1.001 * ylim[1]
+        _ = ax.set_ylim(*ylim)
+        _ = ax.set_xlim(*xlim)
+        # remove yaxis / ticks / ticklabels near bottom
+        ytck = [-0.1 * ymax, 1.001 * ymax]
+        ytl = ax.yaxis.get_ticklocs()
+        _ = ax.spines['left'].set_bounds(*ytck)
+        for sp in ['left', 'bottom']:
+            _ = ax.spines[sp].set_color(axiscol)
+        _ = ax.yaxis.set_ticks(ytl[ytl > ytck[0]])
+        _ = ax.set_ylim(*ylim)  # have to do this twice
+        _ = ax.tick_params(color=tickcol, width=0.5, labelcolor=ticklabcol)
+
+        # annotations
+        yl = 'Effort (AU)'
+        yo = 1 - np.diff(ytck) / np.diff(ylim) / 2.
+        _ = ax.set_ylabel(yl, y=yo, color=axislabcol)
+        _ = ax.set_xlabel('Time (s)', color=axislabcol)
+
+        box_off(ax)
+        ax.patch.set_facecolor('none')
+fig.tight_layout()
+
+'''
+# hatch_between must come after tight_layout to get same angle on all subplots
+for (ax, t, clu, cluster_ymin, cluster_ymax) in signifs:
+    _ = hatch_between(ax, 10, t[clu], cluster_ymin, cluster_ymax,
+                      linewidth=1.5, color='w', zorder=1)
+'''
+
+if savefig:
+    fig.savefig('fig-2.pdf')
+else:
+    plt.ion()
+    plt.show()
