bd5c6885d7c24e306252380f654266e391c99c4f
drammock <dan.mccloy@gmail.com>
2016-06-29 17:09:54 -0700

    models/coefplots done; overhaul / DRY

--- analyze-behavioral-data.R
+++ analyze-behavioral-data.R
@@ -9,14 +9,17 @@
 # License: BSD (3-clause)
 
 library(parallel)
-library(stringi)
 library(lme4)
-library(ez)
+library(dotwhisker)
+# library(stringi)
+# library(ez)
 
 # file paths
 data_dir <- "data-behavioral"
+out_dir <- "models"
 rev_fname <- file.path(data_dir, "rev-behdata-xlongform.tsv")
 voc_fname <- file.path(data_dir, "voc-behdata-xlongform.tsv")
+
 # data types
 common_cols <- c(subj="integer", block="integer", trial="integer",
                  run_index="integer", attn="character", hit="logical",
@@ -24,30 +27,35 @@ common_cols <- c(subj="integer", block="integer", trial="integer",
                  slot="integer", attn_lett="character", mask_lett="character",
                  targ="logical", foil="logical", press="logical", 
                  onset="numeric", press_time="numeric", reax_time="numeric")
+
 # add in the experiment-specific variables
 rev_cols <- c(common_cols, reverb="character", gender="character")
 voc_cols <- c(common_cols, voc_chan="character", gap_len="character")
-# read in data
-rev <- read.delim(rev_fname, sep="\t", colClasses=rev_cols)
-voc <- read.delim(voc_fname, sep="\t", colClasses=voc_cols)
 
+# read in data
+rev_df <- read.delim(rev_fname, sep="\t", colClasses=rev_cols)
+voc_df <- read.delim(voc_fname, sep="\t", colClasses=voc_cols)
+
+
+################################################################################
 # SET FACTOR CONTRASTS
-# treatment
+# treatment contrasts
 txContrast <- function(x, ...) {
     x <- factor(x, ...)
     contrasts(x) <- contr.treatment
     colnames(contrasts(x)) <- paste0("_", levels(x)[-1])
     x
 }
-# sum-to-one
-sumContrast <- function(x, ...) {
+
+# sum-to-one (deviation contrasts)
+devContrast <- function(x, ...) {
     x <- factor(x, ...)
-    stopifnot(length(levels(x)) == 2)
     contrasts(x) <- contr.sum
-    contrasts(x) <- contrasts(x) * 0.5
-    colnames(contrasts(x)) <- paste0("_", levels(x)[1])
+    contrasts(x) <- contrasts(x) / 2
+    colnames(contrasts(x)) <- paste0("_", levels(x)[-length(levels(x))])
     x
 }
+
 # common variables
 makeFactors <- function(df) {
     df <- within(df, {
@@ -55,80 +63,124 @@ makeFactors <- function(df) {
         truth <- ifelse(targ, "target", ifelse(foil, "foil", "neither"))
         truth <- txContrast(truth, levels=c("neither", "target", "foil"))
         # attn
-        attn <- sumContrast(attn, levels=c("maint.", "switch"))
-        # half
-        half <- 1 + as.integer(slot > 1)
-        half <- sumContrast(as.character(half), levels=c("1", "2"))
+        attn <- devContrast(attn, levels=c("maint.", "switch"))
         # slot
-        slot <- txContrast(as.character(slot), levels=c("0", "1", "2", "3"))
+        slot <- slot + 1  # reset to 1-indexed instead of 0-indexed
+        slot <- txContrast(as.character(slot), levels=c("1", "2", "3", "4"))
     })
     df
 }
-rev <- makeFactors(rev)
-voc <- makeFactors(voc)
+rev_df <- makeFactors(rev_df)
+voc_df <- makeFactors(voc_df)
+
 # experiment-specific variables
-rev <- within(rev, {
-    reverb <- sumContrast(reverb, levels=c("anech.", "reverb"))
-    gender <- sumContrast(gender, levels=c("MF", "MM"))
+rev_df <- within(rev_df, {
+    reverb <- devContrast(reverb, levels=c("anech.", "reverb"))
+    gender <- devContrast(gender, levels=c("MF", "MM"))
 })
-voc <- within(voc, {
-    voc_chan <- sumContrast(voc_chan, levels=c("20", "10"))
-    gap_len <- sumContrast(gap_len, levels=c("long", "short"))
+voc_df <- within(voc_df, {
+    voc_chan <- devContrast(voc_chan, levels=c("20", "10"))
+    gap_len <- devContrast(gap_len, levels=c("long", "short"))
 })
+
+# redefine "truth" to be just target & non-target (don't model foils)
+rev_df2 <- rev_df
+voc_df2 <- voc_df
+rev_df2$truth <- txContrast(rev_df2$targ, levels=c(FALSE, TRUE),
+                            labels=c("non-target", "target"))
+voc_df2$truth <- txContrast(voc_df2$targ, levels=c(FALSE, TRUE),
+                            labels=c("non-target", "target"))
+
 # make sure factor creation didn't generate NAs
-invisible(within(rev, {
-    for (var in c(reverb, gender, attn, slot, half)) stopifnot(all(!is.na(var)))
+invisible(within(rev_df, {
+    for (var in c(reverb, gender, attn, slot)) stopifnot(all(!is.na(var)))
 }))
-invisible(within(voc, {
-    for (var in c(voc_chan, gap_len, attn, slot, half)) stopifnot(all(!is.na(var)))
+invisible(within(rev_df2, {
+    for (var in c(reverb, gender, attn, slot)) stopifnot(all(!is.na(var)))
+}))
+invisible(within(voc_df, {
+    for (var in c(voc_chan, gap_len, attn, slot)) stopifnot(all(!is.na(var)))
+}))
+invisible(within(voc_df2, {
+    for (var in c(voc_chan, gap_len, attn, slot)) stopifnot(all(!is.na(var)))
 }))
 
 
-# MODELS: REVERB
-# set up formulae to be tested
+################################################################################
+# DEFINE MODELS
 formulae <- list(
-    null_model=formula(press ~ truth + (1|subj)),
-    full_model=formula(press ~ truth*reverb*gender*attn + attn*slot + (1|subj)),
-    half_model=formula(press ~ truth*reverb*gender*attn + attn*half + (1|subj))
+	# reverb
+    rev_null_model=formula(press ~ truth + (1|subj)),
+    rev_full_model=formula(press ~ truth*reverb*gender*attn + (1|subj)),
+    rev_slot_model=formula(press ~ truth*reverb*gender*attn + truth*attn*slot + (1|subj)),
+    rev_rdux_model=formula(press ~ truth*reverb*gender*attn + truth:attn:slot + (1|subj)),
+    rev_slot_only_model=formula(press ~ truth*attn*slot + (1|subj)),
+    # reverb w/o foils
+    rev_targ_model=formula(press ~ truth*reverb*gender*attn + (1|subj)),
+    rev_targ_slot_model=formula(press ~ truth*reverb*gender*attn + truth*attn*slot + (1|subj)),
+    # vocoder
+    voc_null_model=formula(press ~ truth + (1|subj)),
+    voc_full_model=formula(press ~ truth*voc_chan*gap_len*attn + (1|subj)),
+    voc_slot_model=formula(press ~ truth*voc_chan*gap_len*attn + truth*attn*slot*gap_len + (1|subj)),
+    voc_rdux_model=formula(press ~ truth*voc_chan*gap_len*attn + truth:attn:slot:gap_len + (1|subj)),
+    voc_slot_only_model=formula(press ~ truth*attn*slot*gap_len + (1|subj)),
+    # vocoder w/o foils
+    voc_targ_model=formula(press ~ truth*voc_chan*gap_len*attn + (1|subj)),
+    voc_targ_slot_model=formula(press ~ truth*voc_chan*gap_len*attn + truth*attn*slot*gap_len + (1|subj))
 )
-# fit models
-rev_models <- mapply(function(formula_, name) {
-    mod <- glmer(formula_, data=rev, family=binomial(link="probit"),
-                 control=glmerControl(optCtrl=list(maxfun=25000)))
-    cat(paste("reverb", name, "finished\n"))
-    mod
-    }, formulae, names(formulae))
+datas <- c(rep(list(rev_df), 5), rep(list(rev_df2), 2), 
+           rep(list(voc_df), 5), rep(list(voc_df2), 2))
+stopifnot(length(datas) == length(formulae))
 
 
-# MODELS: VOCODER
-# set up formulae to be tested
-formulae <- list(
-    null_model=formula(press ~ truth + (1|subj)),
-    full_model=formula(press ~ truth*voc_chan*gap_len*attn + attn*slot*gap_len
-                       + (1|subj))
-)
-# fit models
-voc_models <- mapply(function(formula_, name) {
-    mod <- glmer(formula_, data=voc, family=binomial(link="probit"),
-                 control=glmerControl(optCtrl=list(maxfun=25000)))
-    cat(paste("vocoder", name, "finished\n"))
+################################################################################
+# FIT MODELS
+cat("fitting models\n")
+models <- mcmapply(function(formula_, name, data_source) {
+    mod <- glmer(formula_, data=data_source, family=binomial(link="probit"),
+                 control=glmerControl(optCtrl=list(maxfun=30000)))
+    cat(paste(name, "finished\n"))
     mod
-    }, formulae, names(formulae))
+    }, formulae, names(formulae), datas)
+save(models, file=file.path(out_dir, "all-models.RData"))
+
+
+################################################################################
+# SORT MODELS BY CONVERGENCE
+# valid
+converged <- models[sapply(models, function(i) {
+    is.null(i@optinfo$conv$lme4$code)
+})]
+
+# invalid
+why_nonconverged <- sapply(models, function(i) {
+    ifelse(is.null(i@optinfo$conv$lme4$code), NA,
+           list(i@optinfo$conv$lme4$messages))
+})
+why_nonconverged <- why_nonconverged[!is.na(why_nonconverged)]
+sink(file.path(out_dir, "convergence-failures.txt"))
+print(why_nonconverged)
+sink()
+
+# split by experiment
+converged_rev <- converged[substr(names(converged), 1, 3) %in% "rev"]
+converged_voc <- converged[substr(names(converged), 1, 3) %in% "voc"]
 
 
 stop()
-
+################################################################################
 # COEFPLOTS
-library(dotwhisker)
-library(ggplot2)
-revplot <- dwplot(rev_models$full_model) + theme_bw()
-vocplot <- dwplot(voc_models$full_model) + theme_bw()
-comboplot <- dwplot(list(reverb=rev_models$full_model,
-                         vocoder=voc_models$full_model)) + theme_bw()
+dwplot(models[c("rev_full_model", "rev_targ_model", "rev_targ_slot_model")])
+dwplot(models[c("voc_full_model", "voc_targ_model", "voc_targ_slot_model")])
 
-stop()
+#plot_rev <- dwplot(converged_rev) + theme_bw()
+#plot_voc <- dwplot(converged_voc) + theme_bw()
+#save(plot_rev, plot_voc, file=file.path(out_dir, "coefplots.Rdata"))
 
-## aggregate
+
+################################################################################
+# LEFTOVERS
+# aggregate
 agg_cols <- c("hits", "misses", "false_alarms", "corr_rej")
 by_cols_rev <- c("subj", "attn", "reverb", "gender")
 by_cols_voc <- c("subj", "attn", "bands", "gap_len")
