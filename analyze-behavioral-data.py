478680e744a549007b2989f8b96a77f4e54ea99f
drammock <dan.mccloy@gmail.com>
2016-07-11 17:54:00 -0700

    switch to seaborn, add violin & strip

--- analyze-behavioral-data.py
+++ analyze-behavioral-data.py
@@ -13,18 +13,20 @@ and gender/reverb experiments.
 
 import numpy as np
 import pandas as pd
+import seaborn as sns
 from os import getcwd
 from os import path as op
 from expyfun import analyze as efa
 from matplotlib import rcParams
 from matplotlib import pyplot as plt
+# from matplotlib.colors import colorConverter as cc
 from ast import literal_eval
 
 
 # flags
 plt.ioff()
-savefig = True
-boxplot = True
+savefig = False
+plot_type = 'strip'  # box, bar, strip, violin
 
 # file I/O
 work_dir = getcwd()
@@ -38,6 +40,13 @@ rcp = {'font.sans-serif': ['M+ 1c'], 'font.style': 'normal',
        'pdf.fonttype': 42, 'xtick.top': False, 'ytick.right': False}
 plt.rcdefaults()
 rcParams.update(rcp)
+sns.set_style("white", {'ytick.major.size': 3})
+
+# plot params
+bp = dict(color='none', facecolor='0.7')
+mp = dict(color='w', linewidth=1.5)
+wp = dict(color='k', linestyle='solid')
+fp = dict(color='0.5', markeredgecolor='none', marker='.')
 
 # column selector shorthand
 hmfc = ['hits', 'misses', 'false_alarms', 'corr_rej']
@@ -66,21 +75,27 @@ for ix, fname in enumerate(fnames):
         dpr = agg['dprime'].unstack(0)
         if aggvar[-1] == 'voc_chan':
             dpr = dpr.sort_index(0, ascending=False)
-        if boxplot:
-            bp = dict(color='none', facecolor='0.5')
-            mp = dict(color='w', linewidth=1.5)
-            wp = dict(color='k', linestyle='solid')
-            fp = dict(color='0.5', markeredgecolor='none', marker='.')
-            boxplot_dict = ax.boxplot(dpr.T.values, whis=(5, 95), notch=True,
-                                      bootstrap=5000, patch_artist=True,
-                                      boxprops=bp, flierprops=fp,
-                                      medianprops=mp, whiskerprops=wp)
+        df = pd.melt(dpr.T, value_vars=dpr.index.tolist(),
+                     value_name='d-prime')
+        if plot_type in ['box', 'strip']:
+            wp2 = wp if plot_type == 'box' else dict(linewidth=0)
+            ax = sns.boxplot(x=dpr.index.name, y='d-prime', data=df,
+                             ax=ax, showcaps=False, showfliers=False,
+                             boxprops=bp, medianprops=mp, whiskerprops=wp2)
+            if plot_type == 'strip':
+                ax = sns.stripplot(x=dpr.index.name, y='d-prime', data=df,
+                                   jitter=True, size=3, color='k', linewidth=0,
+                                   ax=ax)
+        elif plot_type == 'violin':
+            sns.violinplot(x=dpr.index.name, y='d-prime', data=df,
+                           palette="Set3", bw=0.5, cut=0.5,
+                           linewidth=1, ax=ax, inner='point')
+        if plot_type in ['box', 'strip', 'violin']:
             ax.xaxis.set_label_text(groupname)
             ax.xaxis.set_ticklabels(dpr.index.tolist())
             ax.set_ylim(0, 5)
             ax.yaxis.set_ticks(range(0, 6))
-            ax.spines['top'].set_visible(False)
-            ax.spines['right'].set_visible(False)
+            sns.despine()
         else:
             ax, bar = efa.barplot(dpr, err_bars='se', ax=ax, groups=[[0, 1]],
                                   group_names=groupname, brackets=brack,
@@ -88,8 +103,9 @@ for ix, fname in enumerate(fnames):
             ax.set_ylim(0, 3.25)
             ax.yaxis.set_ticks(range(0, 4))
         if iix:
-            ax.yaxis.set_visible(False)
-            ax.spines['left'].set_visible(False)
+            pass
+            #ax.yaxis.set_visible(False)
+            #ax.spines['left'].set_visible(False)
         else:
             ax.yaxis.set_label_text('d-prime')
     plt.tight_layout()
