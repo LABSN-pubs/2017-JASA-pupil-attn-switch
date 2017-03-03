320eedcf3c9345631f99cd9bf4cfbfb49be03f81
drammock <dan.mccloy@gmail.com>
2017-03-03 13:33:34 -0800

    refactor analysis scripts

--- analyze-voc-data.py
+++ analyze-voc-data.py
@@ -15,15 +15,18 @@ experiment.
 
 from __future__ import print_function
 import time
-from glob import glob
 from os import getcwd
 from os import path as op
 import numpy as np
-import scipy.signal as ss
 from scipy.io import loadmat
 from pyeparse import read_raw, Epochs
 from pyeparse.utils import pupil_kernel
-from expyfun.analyze import restore_values
+from pupil_helper_functions import (parse_run_indices, get_onset_times,
+                                    get_pupil_data_file_list,
+                                    extract_event_codes, reorder_epoched_data,
+                                    restructure_dims, do_continuous_deconv,
+                                    get_gaze_angle)
+
 
 # flags
 downsample = False
@@ -38,28 +41,32 @@ param_file = op.join(data_dir, 'orderMain.mat')
 # params
 subjects = ['01', '02', '04', '6', '7', '8', '10', '11',
             '12', '13', '14', '55', '96', '97', '98', '99']
-runs = 10
+n_blocks = 10
 t_min, t_max = -0.5, 6.05  # trial epoch extents
 deconv_time_pts = None
-peak_vals = [0.512]  # t_max of pupil response kernel estimated at LABS^N; see
-                     # http://dx.doi.org/10.1121/1.4943787    (analysis:ignore)
+t_peak = 0.512  # t_max of pupil response kernel estimated at LABS^N; see
+                # http://dx.doi.org/10.1121/1.4943787    (analysis:ignore)
 fs_in = 1000.0
 fs_out = 25.  # based on characterize-freq-content.py; no appreciable energy
               # above 3 Hz in average z-score data or kernel  (analysis:ignore)
 fs_out = fs_out if downsample else fs_in
 
+# physical details of the eyetracker/screen setup
+# (for calculating gaze deviation from fixation cross in degrees)
+screenprops = dict(dist_cm=50., width_cm=53., height_cm=29.8125,
+                   width_px=1920, height_px=1080)
+
 # load trial info
 bm = loadmat(param_file)
 run_inds = bm['runInds']
-big_mat = bm['bigMat']
+stim_indices, bands = parse_run_indices(run_inds, n_blocks)
 '''
 comment from MATLAB (makeSoundFiles.m, line 133) about bigMat:
 bigMat = []; % Attn switch (0=no, 1=yes), midGapInd,
                initial target (talker index), inital masker (talker index),
                cueType, TargMod, MaskMod, Mask Multiplier, Targ pos, Mask pos
 '''
-assert len(run_inds) == runs
-run_inds = [np.array([ridx[0][0], ridx[0][1]], int).T for ridx in run_inds]
+big_mat = bm['bigMat']
 
 # construct event dict (mapping between trial parameters and integer IDs)
 event_dict = dict()
@@ -69,32 +76,25 @@ for sn, sw in zip([100, 200], ['M', 'S']):        # maintain / switch
             event_dict.update({'x'.join([sw, gs, bs]): sn + gn + bn})
 
 # init some containers
-fits = list()
 zscores = list()
+zscores_structured = list()
+fits = list()
+fits_structured = list()
 fits_continuous = list()
-kernels = list()
+gaze_angles = list()  # relative to fixation cross
 
-# pre-calculate kernels
-for peak in peak_vals:
-    kernel = pupil_kernel(fs_out, t_max=peak, dur=2.0)
-    kernels.append(kernel)
+# pre-calculate kernel
+kernel = pupil_kernel(fs_out, t_max=t_peak, dur=2.0)
 
 for subj in subjects:
     t0 = time.time()
-    print('Subject {}...'.format(subj))
-    subj_data_dir = glob(op.join(data_dir, 'subj{}_*_el'.format(subj)))
-    assert len(subj_data_dir) == 1
-    subj_data_dir = subj_data_dir[0]
-    fnames = sorted(glob(op.join(subj_data_dir, 'subj{}_*.edf'.format(subj))))
-    assert len(fnames) in [13, 14, 15]
-    fnames = fnames[-10:]
-    subj_mat = glob(op.join(data_dir, 'subj{}_*.mat'.format(subj)))
-    assert len(subj_mat) == 1
-    subj_mat = loadmat(subj_mat[0])
-    time_vecs = subj_mat['timeVecs'][-10:]
-    time_vecs = [t[0][:, 1] for t in time_vecs]  # column 1 is "sound onset"
     raws = list()
     events = list()
+    print('Subject {}...'.format(subj))
+    # load stim times from MAT file of trial params / behavioral responses
+    stim_onset_times = get_onset_times(subj, data_dir)
+    # find files for this subj
+    fnames = get_pupil_data_file_list(subj, data_dir)
     print('  Loading block', end=' ')
     for ri, fname in enumerate(fnames):
         print(str(ri + 1), end=' ')
@@ -102,39 +102,15 @@ for subj in subjects:
         assert raw.info['sfreq'] == fs_in
         raw.remove_blink_artifacts()
         raws.append(raw)
-        # convert 1-based trial numbers (matlab) to 0-based
-        stim_nums = run_inds[ri][:, 0] - 1
-        small_mat = big_mat[stim_nums]
-        # TRIALID 3 -> a real trial (0, 1, and 2 are types of training trials)
-        event = raw.find_events('TRIALID 3', 1)
-        eyelink_stim_ord = [int(m[1].split(',')[3]) - 1
-                            for m in raw.discrete['messages']
-                            if 'TRIALID 3' in m[1]]
-        # check for missing trials
-        n_miss = len(stim_nums) - len(event)
-        if n_miss > 0:
-            missing = list()
-            jj = 0
-            for ti, tnum in enumerate(stim_nums):
-                if eyelink_stim_ord[jj] == tnum:
-                    jj += 1
-                else:
-                    missing.append(ti)
-            not_missing = np.setdiff1d(np.arange(len(stim_nums)), missing)
-            assert len(missing) == n_miss
-            samps = restore_values(time_vecs[ri], event[:, 0], missing)
-            event = np.array((samps[0], np.ones(samps[0].size)), int).T
-            old_ord = eyelink_stim_ord
-            eyelink_stim_ord = np.empty_like(stim_nums)
-            eyelink_stim_ord[missing] = stim_nums[missing]
-            eyelink_stim_ord[not_missing] = old_ord
-            print('Recovered {} trial(s)'.format(n_miss), end='\n    ')
-        assert np.array_equal(eyelink_stim_ord, stim_nums)
-        assert len(event) == len(stim_nums)
-        # convert event numbers
-        band_num = run_inds[ri][:, 1]
-        gap_num = 10 * small_mat[:, 1]    # cf. line 251 of vocExperiment_v2.m
-        attn_num = 100 * small_mat[:, 0]  # cf. line 250 of vocExperiment_v2.m
+        # get the stimulus numbers presented in this block
+        this_stim_nums = stim_indices[ri]
+        # extract event codes from eyelink data
+        event = extract_event_codes(raw, this_stim_nums, stim_onset_times, ri)
+        # convert event IDs; cf. lines 250-251 of vocExperiment_v2.m
+        # showing which dimensions correspond to gap & attn
+        band_num = bands[ri]
+        gap_num = 10 * big_mat[this_stim_nums, 1]
+        attn_num = 100 * big_mat[this_stim_nums, 0]
         event[:, 1] = band_num + gap_num + attn_num
         events.append(event)
 
@@ -143,100 +119,64 @@ for subj in subjects:
     if downsample:
         print('  Downsampling...')
         epochs.resample(fs_out, n_jobs=n_jobs)
+    # compute gaze angles (in degrees)
+    angles = get_gaze_angle(epochs, screenprops)
+    # put zscored pupil size in same order as big_mat
+    # (sequential by stimulus ID)
     zscore = epochs.pupil_zscores()
-
+    zscore_ord = reorder_epoched_data(zscore, big_mat, bands, stim_indices,
+                                      epochs.n_times)
+    # now reshape by condition (trial, gap, attn, bands, time)
+    zscore_struct = restructure_dims(zscore_ord, big_mat, bands,
+                                     epochs.n_times)
     # init some containers
     kernel_fits = list()
     kernel_zscores = list()
     kernel_fits_continuous = list()
 
-    for kernel in kernels:
-        print('  Deconvolving...')
-        fit, time_pts = epochs.deconvolve(kernel=kernel, n_jobs=n_jobs,
-                                          acc=1e-3)
-        zscore = epochs.pupil_zscores()
-        if deconv_time_pts is None:
-            deconv_time_pts = time_pts
-        assert np.array_equal(deconv_time_pts, time_pts)
-
-        # reorder fits to match big_mat (stim# in serial order, voc band, time)
-        order = np.array(run_inds)
-        band_idx = (order[:, :, 1] - 1).ravel()
-        stim_idx = (order[:, :, 0] - 1).ravel()  # convert 1-based to 0-based
-        fits_ordered = np.inf * np.ones((len(big_mat), 2,
-                                         len(deconv_time_pts)))
-        zscores_ordered = np.inf * np.ones((len(big_mat), 2, epochs.n_times))
-        fits_ordered[stim_idx, band_idx, :] = fit
-        zscores_ordered[stim_idx, band_idx, :] = zscore
-        assert not np.any(fits_ordered == np.inf)
-        assert not np.any(zscores_ordered == np.inf)
-
-        # now break up by condition (trial, gap, attn, bands, time)
-        n_attn = len(np.unique(big_mat[:, 0]))
-        n_gaps = len(np.unique(big_mat[:, 1]))
-        n_band = len(np.unique(np.array(run_inds)[:, :, 1]))
-        trials_per_cond = len(big_mat) / n_attn / n_gaps
-        fits_structured = np.empty((trials_per_cond, n_gaps, n_attn, n_band,
-                                    len(deconv_time_pts)))
-        zscores_structured = np.empty((trials_per_cond, n_gaps, n_attn, n_band,
-                                       epochs.n_times))
-        for ai in range(n_attn):
-            for gi in range(n_gaps):
-                ix = np.logical_and(big_mat[:, 0] == ai + 1,
-                                    big_mat[:, 1] == gi + 1)
-                assert sum(ix) == trials_per_cond
-                fits_structured[:, gi, ai, :, :] = fits_ordered[ix, :, :]
-                zscores_structured[:, gi, ai, :, :] = zscores_ordered[ix, :, :]
-
-        # continuous deconvolution
-        if run_continuous_deconv:
-            # zero padding
-            kernel_nsamp = np.round(kernel.shape[-1]).astype(int)
-            zeropad = np.zeros(zscores_structured.shape[:-1] + (kernel_nsamp,))
-            zscores_zeropadded = np.c_[zeropad, zscores_structured, zeropad]
-            print('  Continuous deconvolution...')
-            len_deconv = zscores_zeropadded.shape[-1] - kernel_nsamp + 1
-            t_cont = epochs.times[:len_deconv - 2 * kernel_nsamp]  # no zeropad
-            fit_continuous_deconv = np.empty(zscores_zeropadded.shape[:-1] +
-                                             (len_deconv,))
-            fit_continuous_deconv[:] = np.inf
-            # do deconvolution
-            for _trial in range(zscores_zeropadded.shape[0]):
-                for _gap in range(zscores_zeropadded.shape[1]):
-                    for _attn in range(zscores_zeropadded.shape[2]):
-                        for _band in range(zscores_zeropadded.shape[3]):
-                            signal = zscores_zeropadded[_trial, _gap, _attn,
-                                                        _band, :]
-                            (fit_continuous_deconv[_trial, _gap, _attn,
-                                                   _band, :],
-                             _) = ss.deconvolve(signal, kernel)
-            assert not np.any(fit_continuous_deconv == np.inf)
-            # remove zero padding
-            fit_continuous_deconv = \
-                fit_continuous_deconv[:, :, :, :, kernel_nsamp:-kernel_nsamp]
-        # combine results from different kernels
-        kernel_fits.append(fits_structured)
-        kernel_zscores.append(zscores_structured)
-        if run_continuous_deconv:
-            kernel_fits_continuous.append(fit_continuous_deconv)
-    # finish subject
-    fits.append(kernel_fits)
-    zscores.append(kernel_zscores)
+    print('  Deconvolving...')
+    deconv_kwargs = dict(kernel=kernel, n_jobs=n_jobs, acc=1e-3)
+    if deconv_time_pts is not None:
+        deconv_kwargs.update(dict(spacing=deconv_time_pts))
+    fit, time_pts = epochs.deconvolve(**deconv_kwargs)
+    if deconv_time_pts is None:
+        deconv_time_pts = time_pts
+    assert np.array_equal(deconv_time_pts, time_pts)
+    # put deconvolved pupil size in same order as big_mat
+    # (sequential by stimulus ID)
+    fit_ord = reorder_epoched_data(fit, big_mat, bands, stim_indices,
+                                   len(deconv_time_pts))
+    # now reshape by condition (trial, gap, attn, bands, time)
+    fit_struct = restructure_dims(fit_ord, big_mat, bands,
+                                  len(deconv_time_pts))
+    # continuous deconvolution
     if run_continuous_deconv:
-        fits_continuous.append(kernel_fits_continuous)
+        (cont_deconv_struct,
+         cont_deconv_times) = do_continuous_deconv(zscore_struct, kernel,
+                                                   epochs.times)
+        fits_continuous.append(cont_deconv_struct)
+    # append this subject's data to global vars
+    fits.append(fit)
+    zscores.append(zscore)
+    gaze_angles.append(angles)
+    fits_structured.append(fit_struct)
+    zscores_structured.append(zscore_struct)
     print('  Done: {} sec.'.format(str(round(time.time() - t0, 1))))
+# convert to arrays
+fits_array = np.array(fits)
+fits_struct_array = np.array(fits_structured)
+zscores_array = np.array(zscores)
+zscores_struct_array = np.array(zscores_structured)
+gaze_angles = np.array(gaze_angles)
 
-fits_nopress = np.array([f[0] for f in fits])
-zscores_nopress = np.array([z[0] for z in zscores])
-
-out_nopress = dict(kernel=kernels[0], fits=fits_nopress,
-                   zscores=zscores_nopress)
-out_common = dict(fs=fs_out, subjects=subjects, t_fit=deconv_time_pts)
+# params to output for all kernels
+out_dict = dict(fs=fs_out, subjects=subjects, t_fit=deconv_time_pts,
+                kernel=kernel, fits=fits_array, zscores=zscores_array,
+                fits_struct=fits_struct_array, angles=gaze_angles,
+                zscores_struct=zscores_struct_array, times=epochs.times)
 if run_continuous_deconv:
-    out_common.update(dict(t_cont=t_cont))
-out_nopress.update(out_common)
-if run_continuous_deconv:
-    fits_continuous_nopress = np.array([f[0] for f in fits_continuous])
-    out_nopress.update(dict(fits_cont=fits_continuous_nopress))
+    fits_continuous_array = np.array(fits_continuous)
+    out_dict.update(dict(cont_fits_struct=fits_continuous_array,
+                         cont_deconv_times=cont_deconv_times))
 
-np.savez_compressed(op.join(work_dir, 'voc_data.npz'), **out_nopress)
+np.savez_compressed(op.join(work_dir, 'voc_data.npz'), **out_dict)
